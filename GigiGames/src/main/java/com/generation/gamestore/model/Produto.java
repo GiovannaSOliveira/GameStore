7d9adc1ae9b0f511201cd1f4396cb074f339e7c9
Giovanna Oliveira <giovannasoliveira58@gmail.com>
2024-01-17 15:07:22 -0300

    Backend
    Criação do Backend com Java Spring e MySQL

---
+++ GigiGames/src/main/java/com/generation/gamestore/model/Produto.java
@@ -0,0 +1,97 @@
+package com.generation.gamestore.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JsonIgnoreProperties ("Produto")
+	private Categoria categoria;
+	
+	@NotBlank
+	@Size (min = 5, max = 100, message = "O nome da postagem deve ter entre 5 e 100 caracteres.")
+	private String nome;
+	
+	@Size (min = 5, max = 300, message = "A descrição do produto deve ter de 5 a 300 caracteres.")
+	private String descricao;
+	
+	private long estoque;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@NotNull(message = "Preço é obrigatório.")
+	@Positive(message = "O preço deve ter valor positivo.")
+	private BigDecimal preco;
+
+	
+	//getters e setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public long getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(long estoque) {
+		this.estoque = estoque;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+}
